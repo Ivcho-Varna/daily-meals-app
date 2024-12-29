c6cbf1abebda3643400d2758b661bc668057654d
Ivo Georgiev <ivo.georgiev.varna@gmail.com>
2024-12-30 00:12:37 +0200

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: 'https://ivcho-varna.github.io/daily-meals-app/',
+    base: '/daily-meals-app/',
     plugins: [vue(), vueDevTools()],
     resolve: {
         alias: {
